2b367160488a128bba9963cfb344a845552eee68
Jonasz Mielke <jonasz.mielke@gmail.com>
2026-06-16 16:06:06 +0200

    Early front configuration

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,35 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    ...nextVitals,
+    ...nextTs,
+    tseslint.configs.recommended,
 
-export default eslintConfig;
+    {
+        rules: { curly: 'off' },
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            curly: 'off',
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'react-hooks/exhaustive-deps': 'off',
+            'react-hooks/set-state-in-effect': 'off',
+        },
+    },
+
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts',
+        'design',
+    ]),
+])
+
+export default eslintConfig
